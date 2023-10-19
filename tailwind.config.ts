79f532703c9d30b76299938467f8f6ba87f2d897
Joilton Alvino <joiltonEstTrab@outlook.com>
2023-10-19 15:12:45 -0300

    add project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,28 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['var(--font-inter)'],
+        mono: ['var(--font-ubuntu)'],
       },
+      colors: {
+        blueberry: {
+          300: '#323842',
+          600: '#151A2A',
+          900:'#07061D'
+        },
+        'cotton-candy': '#66D9EF',
+        lemon: '#2DFF0B',
+        gum: '#FD78C4',
+        background : '#030712',
+        pistachio : '#D3FFCC',
+        cream : '#E2DFCD',
+        foam : '#D7DAE2',
+        snow : '#FFFFFF',
+        '8-lemon' : '#0F140F',
+        '20-guava' : '#2C332B',
+        '80-snow' : '#CCCCCC',
+      }
     },
   },
   plugins: [],
